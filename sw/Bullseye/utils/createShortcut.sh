f135ffa8565fc9d517141abf9f86ea7cf09576bb
Martin Lundh <mlundh@gmail.com>
2024-08-04 16:57:04 +0200

    Add .desctop files.

---
+++ sw/Bullseye/utils/createShortcut.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#cp ~/BLEtarget/sw/Bullseye/utils/target.desktop ~/.local/share/applications/
+cp ~/BLEtarget/sw/Bullseye/utils/target.desktop ~/Desktop 
+cp ~/BLEtarget/sw/Bullseye/utils/updateBullseye.desktop ~/Desktop 
+
--- sw/Bullseye/utils/updateBullseye.sh
+++ sw/Bullseye/utils/updateBullseye.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo "Updating Bullseye"
 echo "Hold on ..."
 
